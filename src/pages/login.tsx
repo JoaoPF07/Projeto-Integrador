12d76e4aad305e33d8c1d4e628f27fa08d8068e4
LuisHenrique <luis.hgrodrigues@senacsp.edu.br>
2023-05-26 21:03:47 -0300

    teste

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,52 +1,37 @@
-import { Form ,FormGroup, Input, Label, Button } from "reactstrap";
+import { Form, FormGroup, Input, Label, Button } from "reactstrap";
 import Cabecalho from "../componentes/cabecalho";
 import Rodape from "../componentes/rodape";
 
-
-
-
-function Login (){
-
+function Login() {
   return (
     <div>
-      <div className="cabecalho">
-      Login
-    
-      </div>
+      <div className="cabecalho">Login</div>
       <br />
       <br />
       <Form>
-    <FormGroup floating>
-      <Input
-        id="exampleEmail"
-        name="email"
-        placeholder="Email"
-        type="email"
-      />
-      <Label for="exampleEmail">
-        Email
-      </Label>
-    </FormGroup>
-    {' '}
-    <FormGroup floating>
-      <Input
-        id="examplePassword"
-        name="password"
-        placeholder="Password"
-        type="password"
-      />
-      <Label for="examplePassword">
-        Password
-      </Label>
-    </FormGroup>
-    {' '}
-    <Button>
-      Submit
-    </Button>
-  </Form>
-      <Rodape/>
+        <FormGroup floating>
+          <Input
+            id="exampleEmail"
+            name="email"
+            placeholder="Email"
+            type="email"
+          />
+          <Label for="exampleEmail">Email</Label>
+        </FormGroup>{" "}
+        <FormGroup floating>
+          <Input
+            id="examplePassword"
+            name="password"
+            placeholder="Password"
+            type="password"
+          />
+          <Label for="examplePassword">Password</Label>
+        </FormGroup>{" "}
+        <Button>Submit</Button>
+      </Form>
+      <Rodape />
     </div>
-  )
+  );
 }
 
 export default Login;
