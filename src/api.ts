cdae76971d5625d9cebcac1e70f2b51a32fcc2be
THIAGO GABRIEL DE PADUA <thiago.gpadua1@senacsp.edu.br>
2023-07-05 22:04:50 -0300

    Atualização da api

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,131 @@
 export const api = {
 
-    
+   CarregarEntidadeIndividual: async () => {
+      let response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+      let json = await response.json();
+      return json;
+
+   },
 
 
-  CarregarTodasEntidades: async () => {
+   CarregarTodasEntidades: async (param: string) => {
+      let response = await fetch('' + param);
+      let json = await response.json();
+      return json
+   },
 
-  },
+   AdicionarEntidades: async (
+      email: string,
+      senha: string,
+      nome: string,
+      cnpj: bigint,
+      endereco: string,
+      complemento: string,
+      cidade: string,
+      estado: string,
+      cep: bigint) => {
 
-  CarregarEntidadeIndividual: async () => {
+      let response = await fetch('',
+         {
+            method: 'POST',
+            body: JSON.stringify
+               ({
+                  email,
+                  senha,
+                  nome,
+                  cnpj,
+                  endereco,
+                  complemento,
+                  cidade,
+                  estado,
+                  cep,
 
-  },
+               }),
+            headers: {
+               'content-Type': 'application/json'
+            }
+
+         });
+      let json = await response.json()
+
+      console.log(json);
+      return json;
+
+   },
+
+   AlterarEntidades: async (
+      email: string,
+      senha: string,
+      nome: string,
+      cnpj: bigint,
+      endereco: string,
+      complemento: string,
+      cidade: string,
+      estado: string,
+      cep: bigint) => {
+
+      let response = await fetch('',
+         {
+            method: 'PUT',
+            body: JSON.stringify
+
+               ({
+                  email,
+                  senha,
+                  nome,
+                  cnpj,
+                  endereco,
+                  complemento,
+                  cidade,
+                  estado,
+                  cep,
+
+               }),
+            headers: {
+               'content-Type': 'application/json'
+            }
+         }
+
+
+
+      )
+   },
+
+   DeletarEntidades: async (
+      email: string,
+      senha: string,
+      nome: string,
+      cnpj: bigint,
+      endereco: string,
+      complemento: string,
+      cidade: string,
+      estado: string,
+      cep: bigint) => {
+
+      let response = await fetch('',
+         {
+            method: 'DELETE',
+            body: JSON.stringify
+
+               ({
+                  email,
+                  senha,
+                  nome,
+                  cnpj,
+                  endereco,
+                  complemento,
+                  cidade,
+                  estado,
+                  cep,
+
+               }),
+            headers: {
+               'content-Type': 'application/json'
+            }
+         }
+
+
+
+      )}
+   };
   
-};
